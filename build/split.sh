8056eb53da7ade0c9c95013eebe32d3f271e5cf6
Brant Hwang <dlstj3039@gmail.com>
2017-03-18 22:35:25 +0900

    Update Site

--- build/split.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-git subsplit init https://github.com/axisj/axisj.com.git
-git subsplit publish --heads="master" --no-tags _site:https://github.com/axisj/axisj.com_site.git
-rm -rf .subsplit/
