f54f3c7bf1584d758413fc903fbf4239bd1b7e32
Brant Hwang <dlstj3039@gmail.com>
2017-08-03 15:20:34 +0900

    Update Site

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ jekyll build
 git add -A
 git commit -m "Update Site"
 git push origin master
-git subtree push --prefix  _site/ origin gh-pages --force
+
+git push origin `git subtree split --prefix _site gh-pages`:gh-pages2 --force
