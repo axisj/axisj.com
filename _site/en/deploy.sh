32f1e104e408641fc62fef06bcff7885aaf39e09
Brant Hwang <dlstj3039@gmail.com>
2017-08-03 15:20:54 +0900

    Update Site

--- _site/en/deploy.sh
+++ _site/en/deploy.sh
@@ -4,4 +4,4 @@ git add -A
 git commit -m "Update Site"
 git push origin master
 
-git push origin `git subtree split --prefix _site gh-pages`:gh-pages2 --force
+git push origin `git subtree split --prefix _site gh-pages`:gh-pages --force
